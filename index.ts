c59e09e63bcd0ed7c08d05c70dd11f8da1a054a2
Jonah Meijers <jonahmeijers97@gmail.com>
2022-07-26 18:47:32 +0200

    formatting

--- index.ts
+++ index.ts
@@ -4,18 +4,10 @@ import { ApiTypes } from './types'
 main()
 
 async function main() {
-	const directus =
-		new Directus<ApiTypes>('http://...')
-	const pages = await directus
-		.items('pages')
-		.readByQuery({
-			fields: [
-				'id',
-				'metadata.title',
-				'metadata.description',
-				'metadata.slug',
-			],
-		})
+	const directus = new Directus<ApiTypes>('http://...')
+	const pages = await directus.items('pages').readByQuery({
+		fields: ['id', 'metadata.title', 'metadata.description', 'metadata.slug'],
+	})
 	const page = pages.data?.[0]
 	page?.metadata?.slug
 }
